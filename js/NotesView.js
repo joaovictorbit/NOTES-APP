f9af82447ebaafefc277f39855222a90a60c8372
joaovictorbit <joaovictorbittencourt96@gmail.com>
2022-08-16 21:43:19 -0300

    Develope the add, delete , side bar of past notes

--- js/NotesView.js
+++ js/NotesView.js
@@ -1,5 +1,92 @@
-export default  class NotesView{
+class NotesView{
     constructor(root,{ onNoteSelect, onNoteAdd, onNoteEdit,onNoteDelete }={}){
-        
+        this.root = root;
+        this.onNoteSelect = onNoteSelect;
+        this.onNoteAdd = onNoteAdd;
+        this.onNoteEdit = onNoteEdit;
+        this.onNoteDelete = onNoteDelete;
+        this.root.innerHTML = `
+            <div class="notes__sidebar">
+                <button class="notes__add" type="button"> Add Note </button>
+                <div class="notes__list"></div>
+            </div>
+            <div class="notes__preview">
+                    <input class="notes__title" type="text" placeholder="New Note">
+                    <textarea class="notes__body">Take Note...</textarea>
+            </div> 
+        `;
+
+        const btnAddNote = this.root.querySelector(".notes__add");
+        const inpTitle = this.root.querySelector(".notes__title");
+        const inpBody = this.root.querySelector(".notes__body");
+
+        btnAddNote.addEventListener("click",() => {
+            this.onNoteAdd();
+        });
+
+        [inpTitle, inpBody].forEach(inputField => {
+            inputField.addEventListener( "blur",() => {
+            const updatedTitle = inpTitle.value.trim();
+            const updatedBody = inpBody.value.trim();
+
+            this.onNoteEdit(updatedTitle,updatedBody);
+        });
+        });
+
+        //To do: hide the note preview by default
+
     }
+
+    _createListItemHTML(id,title, body,updated){
+        const MAX_BODY_LENGTH = 60;
+
+        return `
+        <div class="notes__list-item" data-note-id="${id}">
+            <div class="notes__small-title">${title}</div>
+            <div class="notes__small-body">
+            ${body.substring(0,MAX_BODY_LENGTH)}
+            ${body.length > MAX_BODY_LENGTH ? "...": ""}
+            </div>
+            <div class="notes__small-updated">
+            ${updated.toLocaleString(undefined,{ dateStyle: "full", timeStyle:"short" })}</div>
+        
+        </div>
+
+        `
+    }
+
+    updateNoteList(notes){
+        const notesListContainer = this.root.querySelector(".notes__list");
+
+        //Empty list
+        notesListContainer.innerHTML = "";
+
+        for (const note of notes) {
+            const html = this._createListItemHTML(note.id,note.title,note.body, new Date(note.updated));
+            
+            notesListContainer.insertAdjacentHTML("beforeend",html);
+        
+        }
+
+        //Add select/delete events for each list item
+
+        notesListContainer.querySelectorAll(".notes__list-item").forEach(noteListItem =>{
+            noteListItem.addEventListener("click",() =>{
+                this.onNoteSelect(noteListItem.dataset.noteId);
+            });
+
+            noteListItem.addEventListener("dblclick", () =>{
+                const doDelete = confirm("Are you sure you want to delete this note ?");
+                if(doDelete){
+                    this.onNoteDelete(noteListItem.dataset.noteId);
+                }
+            })
+
+        });
+
+        
+
+    }
+
+
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
- 
-NoteAPI.saveNotes({
-    title: "New Note!",
-    body:"I am a new note."
-});
 
-console.log(NoteAPI.getAllNotes());
+const app = document.getElementById("app");
+
+const view = new NotesView(app,{
+     onNoteAdd(){
+        console.log("Let's add a note");
+    },
+    onNoteSelect(id){
+        console.log("Note Selected:" +id);
+    },
+    onNoteDelete(id){
+        console.log("Note DELETED:" +id);
+    },
+
+    onNoteEdit(newTitle,newBody){
+        console.log(newTitle);
+        console.log(newBody);
+    },
+
+    }
+);0
+
+view.updateNoteList(NoteAPI.getAllNotes());
